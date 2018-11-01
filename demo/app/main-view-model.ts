1b46e23f4d9a5920533322e3a11ceaa4abd92115
Piotr Ilski <piotr.ilski@gmail.com>
2018-11-01 05:50:35 +0100

    Update library

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,6 +1,7 @@
-import { Observable } from 'tns-core-modules/data/observable';
+import { Observable, EventData } from 'tns-core-modules/data/observable';
 import { IdtechVp3300 } from 'nativescript-idtech-vp3300';
 
+import { ListView, ItemEventData } from "tns-core-modules/ui/list-view";
 
 export class HelloWorldModel extends Observable {
   public message: string;
@@ -10,6 +11,17 @@ export class HelloWorldModel extends Observable {
     super();
 
     this.idtechVp3300 = new IdtechVp3300();
-    // this.idtechVp3300.setDelegate(this);
+    this.idtechVp3300.onBluetoothAvailableDevicesListUpdate = (devices: BLEDevice) => {
+
+    };
+  }
+
+  onListViewLoaded(args: EventData) {
+    const listView = <ListView>args.object;
+  }
+
+  onItemTap(args: ItemEventData) {
+      const index = args.index;
+      console.log(`Second ListView item tap ${index}`);
   }
 }
