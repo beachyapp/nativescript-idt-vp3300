6692e502eb8328231d236e62cb8d87b9588fe842
Piotr Ilski <piotr.ilski@gmail.com>
2018-12-20 11:51:25 +0100

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -19,10 +19,11 @@ export declare class IdtechVp3300 extends IdtechEmv {
     private emvReaderControl;
     private delegate;
     constructor();
+    setSleepAndPowerOffTimes(sleepTime: number, powerOffTime: number): void;
     connectWithFriendlyName(name: string): boolean;
     connectWithIdentifier(uuidString: string): boolean;
-    readCardData(amount: number, timeout?: number): Promise<void>;
     cancelReadCardData(): void;
+    readCardData(amount: number, timeout?: number): Promise<void>;
 }
 
 export declare class BluetoothDevice {
